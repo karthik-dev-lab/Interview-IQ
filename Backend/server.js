7bc2ac07c35c6c90c6e9b26ecb095afb146f95ed
karthik <karthiikk8348@gmail.com>
2026-03-24 14:39:01 +0530

    port

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,8 @@ const app=require("./src/app")
 const connectDB=require("./src/config/database")
 const dns = require("dns");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
-
+const PORT = process.env.PORT || 3000;
 connectDB();
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("Server running on port 3000")
 })
